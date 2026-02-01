35a1d8e75c2e3ed6d314a9268a4f9fbaa610b44c
theayushgupta08 <ayushkumarshaw980@gmail.com>
2026-02-02 00:49:33 +0530

    Update axios instance base URL to include API path for correct endpoint access in production environment.

--- frontend/src/axiosInstance.js
+++ frontend/src/axiosInstance.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   // baseURL: 'http://localhost:4000/api/',
-  baseURL: 'https://where-is-my-bus-backend.vercel.app/',
+  baseURL: 'https://where-is-my-bus-backend.vercel.app/api/',
 });
 
 export default axiosInstance;
